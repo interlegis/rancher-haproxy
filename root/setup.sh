d85c2f4b6e6faf2acf6cc6a2fd2e9cff882d7dd0
Fabio Rauber <fabiorauber@gmail.com>
2016-10-20 10:09:34 -0200

    Consolidated SSL and non-SSL templates

--- root/setup.sh
+++ root/setup.sh
@@ -14,9 +14,9 @@ for portcfg in ${PORTS[*]}; do
   if [ ! -f "/opt/tools/confd/etc/conf.d/frontend_${port}.cfg.toml" ]; then
     sed -e "s/%port%/$port/g" /opt/tools/confd/etc.tmpl/conf.d/frontend_%port%.cfg.toml > /opt/tools/confd/etc/conf.d/frontend_${port}.cfg.toml
     if [ "$cfg" == "ssl" ]; then 
-      sed -e "s/%port%/$port/g" -e "s/%porthigh%/$porthigh/g" /opt/tools/confd/etc.tmpl/templates/10-frontend_%port%-ssl.cfg.tmpl > /opt/tools/confd/etc/templates/10-frontend_${port}.cfg.tmpl
+      sed -e "s/%port%/$port/g" -e "s/%porthigh%/$porthigh/g" -e "s/%ssl%/true/g" /opt/tools/confd/etc.tmpl/templates/10-frontend_%port%.cfg.tmpl > /opt/tools/confd/etc/templates/10-frontend_${port}.cfg.tmpl
     else 
-      sed -e "s/%port%/$port/g" -e "s/%porthigh%/$porthigh/g" /opt/tools/confd/etc.tmpl/templates/10-frontend_%port%.cfg.tmpl > /opt/tools/confd/etc/templates/10-frontend_${port}.cfg.tmpl
+      sed -e "s/%port%/$port/g" -e "s/%porthigh%/$porthigh/g" -e "s/%ssl%/false/g" /opt/tools/confd/etc.tmpl/templates/10-frontend_%port%.cfg.tmpl > /opt/tools/confd/etc/templates/10-frontend_${port}.cfg.tmpl
     fi
   else
     echo "Configuration for port $port already exists."
